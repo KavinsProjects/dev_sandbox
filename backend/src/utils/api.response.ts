dd9245b227590ba844845dc1a3dd9c8eb4ca8d36
unknown <kavinkanagaraj16@gmail.com>
2026-07-10 13:33:36 +0530

    Writeing API Response

---
+++ backend/src/utils/api.response.ts
@@ -0,0 +1,15 @@
+// class APiResponse {
+//     statusCode : number;
+//     message : string;
+//     data : string;
+//     sucess : number;
+
+//     constructor(statusCode: number = 200, message: string = '', data: string = '', sucess: number = 1) {
+//         this.statusCode = statusCode;
+//         this.message = message;
+//         this.data = data;
+//         this.sucess = sucess;
+//     }
+// }
+
+// export {APiResponse}
